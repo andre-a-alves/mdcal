f546ed91c0be1ac92ed3d43ed09c05c0fe3ab598
Andre A Alves <andre@alves.me>
2025-05-22 10:13:22 +0200

    Refactor for testability

--- cmd/root.go
+++ cmd/root.go
@@ -9,7 +9,6 @@ import (
 	"github.com/andre-a-alves/mdcal/pkg/interactive"
 	"github.com/andre-a-alves/mdcal/pkg/utils"
 	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
 )
 
 var rootCmd = &cobra.Command{
@@ -45,22 +44,24 @@ If no arguments are provided, it runs in interactive mode.`
 	rootCmd.PersistentFlags().BoolP("version", "v", false, "Print version information")
 	rootCmd.PersistentFlags().StringP("justify", "j", "left", "Cell justification: left, center, or right")
 
-	rootCmd.Run = func(cmd *cobra.Command, args []string) {
-		// Handle version flag
+	// handleVersionFlag checks if the version flag is set and prints the version if it is
+	handleVersionFlag := func(cmd *cobra.Command) bool {
 		versionFlag, _ := cmd.Flags().GetBool("version")
 		if versionFlag {
 			fmt.Printf("mdcal v%v\n", getVersion())
-			os.Exit(0)
+			return true
 		}
+		return false
+	}
 
-		// Get flag values
+	// initOptionsFromFlags initializes calendar options from command-line flags
+	initOptionsFromFlags := func(cmd *cobra.Command) calendar.Options {
 		weekStart, _ := cmd.Flags().GetString("start")
 		noWeekNo, _ := cmd.Flags().GetBool("no-week-no")
 		workweek, _ := cmd.Flags().GetBool("workweek")
 		noComment, _ := cmd.Flags().GetBool("no-comment")
 		justify, _ := cmd.Flags().GetString("justify")
 
-		// Initialize options with defaults
 		options := calendar.NewOptions()
 		options.FirstDayOfWeek = utils.ParseWeekday(weekStart)
 		options.ShowCalendarWeek = !noWeekNo
@@ -68,64 +69,90 @@ If no arguments are provided, it runs in interactive mode.`
 		options.ShowComments = !noComment
 		options.Justify = justify
 
-		// Check if any args or flags were provided (excluding help flag)
-		cmd.Flags().Visit(func(f *pflag.Flag) {
-			if f.Name == "help" {
-				return
-			}
-		})
-		noFlagsOrArgs := cmd.Flags().NFlag() == 0 && len(args) == 0
+		return options
+	}
 
-		if noFlagsOrArgs {
+	// shouldRunInteractively determines if the program should run in interactive mode
+	shouldRunInteractively := func(cmd *cobra.Command, args []string) bool {
+		return cmd.Flags().NFlag() == 0 && len(args) == 0
+	}
+
+	// processYearArg processes the year argument if present
+	processYearArg := func(args []string, options *calendar.Options) {
+		if len(args) > 0 {
+			if year, err := strconv.Atoi(args[0]); err == nil {
+				options.Year = year
+			} else {
+				fmt.Println("Invalid year, using current year")
+			}
+		}
+	}
+
+	// processMonthArg processes the month argument if present
+	processMonthArg := func(args []string, options *calendar.Options) {
+		if len(args) > 1 {
+			if month, err := strconv.Atoi(args[1]); err == nil && month >= 1 && month <= 12 {
+				options.Month = &month
+			} else {
+				fmt.Println("Invalid month, generating calendar for the whole year")
+			}
+		}
+	}
+
+	// processDateRangeArgs processes date range arguments if present
+	processDateRangeArgs := func(args []string, options *calendar.Options) {
+		if len(args) == 3 {
+			// If we have 3 args, it's year month endMonth (same year)
+			if endMonth, err := strconv.Atoi(args[2]); err == nil && endMonth >= 1 && endMonth <= 12 {
+				endYear := options.Year
+				options.EndYear = &endYear
+				options.EndMonth = &endMonth
+			} else {
+				fmt.Println("Invalid end month, ignoring range")
+			}
+		} else if len(args) == 4 {
+			// If we have 4 args, it's year month endYear endMonth
+			endYear, errYear := strconv.Atoi(args[2])
+			endMonth, errMonth := strconv.Atoi(args[3])
+
+			if errYear == nil && errMonth == nil && endMonth >= 1 && endMonth <= 12 {
+				options.EndYear = &endYear
+				options.EndMonth = &endMonth
+			} else {
+				if errYear != nil {
+					fmt.Println("Invalid end year, ignoring range")
+				}
+				if errMonth != nil || endMonth < 1 || endMonth > 12 {
+					fmt.Println("Invalid end month, ignoring range")
+				}
+				options.EndYear = nil
+				options.EndMonth = nil
+			}
+		}
+	}
+
+	// processCommandLineArgs processes all command-line arguments
+	processCommandLineArgs := func(args []string, options *calendar.Options) {
+		processYearArg(args, options)
+		processMonthArg(args, options)
+		processDateRangeArgs(args, options)
+	}
+
+	rootCmd.Run = func(cmd *cobra.Command, args []string) {
+		// Handle version flag
+		if handleVersionFlag(cmd) {
+			os.Exit(0)
+		}
+
+		// Initialize options from flags
+		options := initOptionsFromFlags(cmd)
+
+		if shouldRunInteractively(cmd, args) {
 			// Run in interactive mode
 			interactive.RunInteractiveMode(&options)
 		} else {
-			// Handle unnamed arguments (year, month, end year, end month)
-			if len(args) > 0 {
-				if year, err := strconv.Atoi(args[0]); err == nil {
-					options.Year = year
-				} else {
-					fmt.Println("Invalid year, using current year")
-				}
-			}
-
-			if len(args) > 1 {
-				if month, err := strconv.Atoi(args[1]); err == nil && month >= 1 && month <= 12 {
-					options.Month = &month
-				} else {
-					fmt.Println("Invalid month, generating calendar for the whole year")
-				}
-			}
-
-			// Handle range format: year month endMonth or year month endYear endMonth
-			if len(args) == 3 {
-				// If we have 3 args, it's year month endMonth (same year)
-				if endMonth, err := strconv.Atoi(args[2]); err == nil && endMonth >= 1 && endMonth <= 12 {
-					endYear := options.Year
-					options.EndYear = &endYear
-					options.EndMonth = &endMonth
-				} else {
-					fmt.Println("Invalid end month, ignoring range")
-				}
-			} else if len(args) == 4 {
-				// If we have 4 args, it's year month endYear endMonth
-				endYear, errYear := strconv.Atoi(args[2])
-				endMonth, errMonth := strconv.Atoi(args[3])
-
-				if errYear == nil && errMonth == nil && endMonth >= 1 && endMonth <= 12 {
-					options.EndYear = &endYear
-					options.EndMonth = &endMonth
-				} else {
-					if errYear != nil {
-						fmt.Println("Invalid end year, ignoring range")
-					}
-					if errMonth != nil || endMonth < 1 || endMonth > 12 {
-						fmt.Println("Invalid end month, ignoring range")
-					}
-					options.EndYear = nil
-					options.EndMonth = nil
-				}
-			}
+			// Process command-line arguments
+			processCommandLineArgs(args, &options)
 		}
 
 		// Generate and print calendar
